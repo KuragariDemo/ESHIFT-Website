cb6219c6efec4b387ffb05a1e40ee545a7363395
KuragariDemo <demomello110078@gmail.com>
2025-06-02 18:35:30 +0700

    Initial commit

---
+++ Controllers/OurServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShift.Controllers
+{
+    public class OurServices : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
